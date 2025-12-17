b3515bd7f8ed58f2ede6c31e81b3b930ed3b096e
Juan Manuel Torrado <jm.torrado.borrachero@gmail.com>
2025-12-17 22:01:45 +0100

    ajuste de la clase principal y habilitacion de ejecucion asincrona

--- src/main/java/juanma/datalab/DatalabApplication.java
+++ src/main/java/juanma/datalab/DatalabApplication.java
@@ -4,9 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
 
+/*
+ clase principal de arranque de la aplicacion
+ aqui se habilita la ejecucion asincrona del proyecto
+*/
+
 @SpringBootApplication
 @EnableAsync
 public class DatalabApplication {
+
+    // el main desde el que se levanta todo el contexto de spring
     public static void main(String[] args) {
         SpringApplication.run(DatalabApplication.class, args);
     }
