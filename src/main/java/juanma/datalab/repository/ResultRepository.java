2f7b62f7c49baeaf753414a73d718415e2733fd1
Juan Manuel Torrado <jm.torrado.borrachero@gmail.com>
2025-12-17 21:49:17 +0100

    ajuste del repositorio de resultados con soporte de paginacion

--- src/main/java/juanma/datalab/repository/ResultRepository.java
+++ src/main/java/juanma/datalab/repository/ResultRepository.java
@@ -4,6 +4,12 @@ import juanma.datalab.domain.Result;
 import org.springframework.data.domain.*;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+/*
+ repositorio para acceder a los resultados de un job
+ incluye soporte de paginacion
+*/
 public interface ResultRepository extends JpaRepository<Result, Long> {
+
+    // devuelve los resultados de un job de forma paginada
     Page<Result> findByJobId(String jobId, Pageable pageable);
 }
