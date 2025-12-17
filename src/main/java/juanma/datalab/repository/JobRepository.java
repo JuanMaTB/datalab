c09043e0a54256a45397690a472f949109d897ad
Juan Manuel Torrado <jm.torrado.borrachero@gmail.com>
2025-12-17 21:48:35 +0100

    ajuste del repositorio jpa de job

--- src/main/java/juanma/datalab/repository/JobRepository.java
+++ src/main/java/juanma/datalab/repository/JobRepository.java
@@ -3,4 +3,8 @@ package juanma.datalab.repository;
 import juanma.datalab.domain.Job;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+/*
+ repositorio jpa para gestionar la entidad job
+ se usa para persistencia y consultas basicas
+*/
 public interface JobRepository extends JpaRepository<Job, String> {}
