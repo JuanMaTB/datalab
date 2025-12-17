98cb3ad38b0619bcfacef62b0ff9e239dd43ef49
Juan Manuel Torrado <jm.torrado.borrachero@gmail.com>
2025-12-17 21:51:47 +0100

    ajuste de la clase de resultados de analisis

--- src/main/java/juanma/datalab/service/AnalysisResult.java
+++ src/main/java/juanma/datalab/service/AnalysisResult.java
@@ -3,13 +3,26 @@ package juanma.datalab.service;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+/*
+ clase simple para agrupar los resultados de un analisis
+ se usa como salida comun de los analizadores
+*/
 @Getter
 @AllArgsConstructor
 public class AnalysisResult {
 
+    // numero de filas procesadas
     private final int rows;
+
+    // suma total
     private final double sum;
+
+    // media calculada
     private final double avg;
+
+    // valor minimo encontrado
     private final double min;
+
+    // valor maximo encontrado
     private final double max;
 }
